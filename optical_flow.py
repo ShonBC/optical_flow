db8ea6f577ca9ca96c46d626c10630083d1ec7c1
ShonBC <scortes3@umd.edu>
2021-05-08 22:38:04 -0700

    Refined code

--- optical_flow.py
+++ optical_flow.py
@@ -1,15 +1,13 @@
 import numpy as np
 import cv2
 
-def draw_vectors():
+def vector_field():
 
     cap = cv2.VideoCapture(cv2.samples.findFile("Cars On Highway.mp4"))
     ret, frame1 = cap.read()
-    prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    prev = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     hsv = np.zeros_like(frame1)
     hsv[..., 1] = 255
-
-    grid_size = int(25 / 2)
 
     while True:
         
@@ -19,9 +17,9 @@ def draw_vectors():
             break
 
         next_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-        flow = cv2.calcOpticalFlowFarneback(prvs, next_gray, None, 0.5, 3, 25, 3, 5, 1.2, 0)
+        flow = cv2.calcOpticalFlowFarneback(prev, next_gray, None, 0.5, 3, 25, 3, 5, 1.2, 0)
 
-        draw_flow(frame2, next_gray, flow, step=16)
+        draw_vec_field(frame2, next_gray, flow)
             
         cv2.imshow('flow', frame2)
         k = cv2.waitKey(30) & 0xff
@@ -30,25 +28,22 @@ def draw_vectors():
         elif k == ord('s'):
             cv2.imwrite('opticalfb.png', frame2)
             # cv2.imwrite('opticalhsv.png', bgr)
-        prvs = next_gray
+        prev = next_gray
 
-def arrows(mag, ang):
 
-    x = mag * np.cos(ang)
-    y = mag * np.sin(ang)
+def draw_vec_field(frame, img, flow, grid_size=25):
 
-    x_start ,x_end, y_start,y_end = x[0], x[1], y[0], y[1]
+    height, width = img.shape
 
-    return x_start ,x_end, y_start,y_end
+    # Create Grid (Default segments image into square sections with size 25 by 25 pixels)
+    y, x = np.mgrid[grid_size / 2 : height : grid_size, grid_size / 2 : width : grid_size].reshape(2,-1).astype(int)
 
-def draw_flow(frame, img, flow, step=16):
-
-    h, w = img.shape[:2]
-    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+    # Define flow vectors
     fx, fy = flow[y,x].T
     vec = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
-    vec = np.int32(vec + 0.5)
+    vec = np.int32(vec)
 
+    # Draw flow vecotrs with origens at center grid sections)
     cv2.polylines(frame, vec, 0, (0, 255, 0))
     for (x1, y1), (x2, y2) in vec:
         cv2.circle(frame, (x1, y1), 1, (0, 255, 0), -1)
@@ -58,7 +53,7 @@ def flow_filter():
 
     cap = cv2.VideoCapture(cv2.samples.findFile("Cars On Highway.mp4"))
     ret, frame1 = cap.read()
-    prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    prev = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     hsv = np.zeros_like(frame1)
     hsv[..., 1] = 255
 
@@ -70,7 +65,7 @@ def flow_filter():
             break
 
         next_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-        flow = cv2.calcOpticalFlowFarneback(prvs, next_gray, None, 0.5, 3, 25, 3, 5, 1.2, 0)
+        flow = cv2.calcOpticalFlowFarneback(prev, next_gray, None, 0.5, 3, 25, 3, 5, 1.2, 0)
         mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
         # draw_vectors(frame2, mag, ang)
         hsv[..., 0] = ang * 180 / np.pi / 2
@@ -85,9 +80,9 @@ def flow_filter():
         elif k == ord('s'):
             cv2.imwrite('opticalfb.png', frame2)
             cv2.imwrite('opticalhsv.png', bgr)
-        prvs = next_gray
+        prev = next_gray
 
 if __name__ == "__main__":
 
     # flow_filter() 
-    draw_vectors()
+    vector_field()
